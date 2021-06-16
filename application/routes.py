5753e87779c5c8e9c4a1d01c0a2122493af626b6
agray998 <a.gray.4797@gmail.com>
2021-06-16 20:03:36 +0000

    Added app

---
+++ application/routes.py
@@ -0,0 +1,27 @@
+from application import app
+from flask import Flask, request, render_template, url_for
+import requests, json
+from datetime import date
+from matplotlib import pyplot as plt
+import numpy as np
+
+@app.route('/')
+def home():
+    response = requests.get('https://api.coronavirus.data.gov.uk/v2/data?areaType=utla&areaCode=E08000009&metric=newCasesByPublishDateChange&format=json')
+    resp_json = response.json()
+    dates = []
+    newCasesByPublishDateChange = []
+    for entry in resp_json['body']:
+        dates.append(entry['date'])
+        newCasesByPublishDateChange.append(entry['newCasesByPublishDateChange'])
+    dates.reverse()
+    newCasesByPublishDateChange.reverse()
+    seven_day_averages = []
+    averages_dates = []
+    for i in range(6, len(newCasesByPublishDateChange)):
+        seven_day_averages.append(sum(newCasesByPublishDateChange[i-6:i+1])/7)
+        averages_dates.append(dates[i])
+    index = np.arange(len(seven_day_averages)) + 0.3
+    plot = plt.bar(index, seven_day_averages, tick_label = averages_dates)
+    plt.savefig(f"/static/output_graph{date.today()}.png")
+    return render_template('home.html', imgname=f"output_graph{date.today()}.png")
